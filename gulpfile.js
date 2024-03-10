3f7d320dd3c278e6c778ee1c90a2ba9fa53c2727
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2024-03-10 14:53:52 +0300

    add new change

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 const { src, dest, watch, parallel, series} = require('gulp');
 const gulp = require('gulp');
 
-const scss = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean');
-const avif = require('gulp-avif');
+// const avif = require('gulp-avif');
 const webp = require('gulp-webp');
 const imagemin = require('gulp-imagemin');
 const svgSprite = require('gulp-svg-sprite');
@@ -16,41 +16,23 @@ const fonter = require('gulp-fonter');
 const ttf2woff2 = require('gulp-ttf2woff2');
 const include = require('gulp-include');
 const deploy = require('gulp-gh-pages');
+const sourcemaps = require('gulp-sourcemaps');
 
-function pages() {
-  return src('app/pages/*.html')
-    .pipe(include({
-      includePaths: 'app/components'
-    }))
-    .pipe(dest('app'))
-    .pipe(browserSync.stream())
-}
+// function images() {
+//   return src(['app/images/**/*.{png,jpg}', '!app/images/icons/*.svg'])
+//     .pipe(newer('app/images'))
+//     .pipe(avif({ quality: 50 }))
 
-function fonts() {
-  return src('app/fonts/src/*.*')
-    .pipe(fonter({
-      formats: ['woff', 'ttf']
-    }))
-    .pipe(src('app/fonts/*.ttf'))
-    .pipe(ttf2woff2())
-    .pipe(dest('app/fonts'))
-}
+//     .pipe(src('app/images/**/*.{png,jpg}'))
+//     .pipe(newer('app/images'))
+//     .pipe(webp())
 
-function images() {
-  return src(['app/images/**/*.{png,jpg}', '!app/images/icons/*.svg'])
-    .pipe(newer('app/images'))
-    .pipe(avif({ quality: 50 }))
+//     .pipe(src('app/images/**/*.{png,jpg}'))
+//     .pipe(newer('app/images'))
+//     .pipe(imagemin())
 
-    .pipe(src('app/images/**/*.{png,jpg}'))
-    .pipe(newer('app/images'))
-    .pipe(webp())
-
-    .pipe(src('app/images/**/*.{png,jpg}'))
-    .pipe(newer('app/images'))
-    .pipe(imagemin())
-
-    .pipe(dest('app/images'))
-}
+//     .pipe(dest('app/images'))
+// }
 
 function sprite() {
   return gulp.src('app/images/icons/*.svg')
@@ -75,9 +57,11 @@ function scripts() {
 
 function styles() {
   return src('app/scss/style.scss')
+    .pipe(sourcemaps.init())
     .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
     .pipe(concat('style.min.css'))
-    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
 }
@@ -103,24 +87,30 @@ function building() {
   return src([
     'app/css/style.min.css',
     'app/images/**/*.*',
-    '!app/images/*.svg',
-    '!app/images/**/*.html',
-    'app/images/sprite.svg',
-    'app/fonts/*.*',
+    // '!app/images/*.svg',
+    // '!app/images/**/*.html',
+    // 'app/images/sprite.svg',
+    // 'app/fonts/*.*',
     'app/js/*.js',
     'app/**/*.html'
   ], { base: 'app' })
     .pipe(dest('dist'))
 }
 
+function deployToGitHubPages() {
+  return src('./dist/**/*')
+    .pipe(deploy());
+}
+
 exports.scripts = scripts;
 exports.styles = styles;
-exports.fonts = fonts;
-exports.pages = pages;
-exports.images = images;
 exports.sprite = sprite;
 exports.watching = watching;
 exports.building = building;
 
+
+exports.deploy = series(deployToGitHubPages);
 exports.build = series(cleanDist, building);
-exports.default = parallel(styles, scripts, pages, watching);
+exports.default = parallel(styles, scripts, watching);
+
+
